527e907aec96ce43d6317a3e0fe805561b73af71
Gentil Pinto <gentil.junior.gp@gmail.com>
2021-12-20 15:46:21 +0100

    minor changes

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,9 @@
 <script>
-	export let name;
+	export let name = 'Gentil';
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
 <style type="text/scss">
