2d460b036c212a3af13706f8f9e3eddc47cf912a
Gentil Pinto <gentil.junior.gp@gmail.com>
2022-01-02 22:39:11 +0100

    created new component

---
+++ src/components/navbar/Navbar.svelte
@@ -0,0 +1,23 @@
+<nav>
+	<div>
+		<h1>Gentil Pinto</h1>
+		<span>Software Developer</span>
+	</div>
+</nav>
+
+<style lang="scss">
+	nav {
+		padding: 24px;
+
+		div {
+			margin: 0 auto;
+			h1 {
+				font: {
+					size: 1.5em;
+					weight: 800;
+				}
+				line-height: 1em;
+			}
+		}
+	}
+</style>
