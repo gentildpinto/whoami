b25c4c7e6ad0abc9383f811fbef0cccad45b749b
Gentil Pinto <gentil.junior.gp@gmail.com>
2022-01-02 22:39:40 +0100

    minor changes at the app.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,36 @@
+<script>
+	import Navbar from './components/navbar/Navbar.svelte';
+</script>
+
 <main>
-	<h1>I don't know :(</h1>
+	<header>
+		<Navbar />
+	</header>
+
+	<section>
+		<h1>Under Construction...</h1>
+	</section>
 </main>
 
 <style lang="scss">
 	main {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-
 		width: 100%;
 		height: 100%;
 
-		h1 {
-			font-size: 5em;
+		header {
+			display: flex;
+			height: 10%;
+		}
+
+		section {
+			height: 90%;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+
+			h1 {
+				font-size: 5em;
+			}
 		}
 	}
 </style>
