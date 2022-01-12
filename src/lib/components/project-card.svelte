3783deaa6497a36d69c0f11d3e1560b243f3891d
Gentil Pinto <gentil.junior.gp@gmail.com>
2022-01-12 12:39:46 +0100

    Add first component

---
+++ src/lib/components/project-card.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let src = ''
+    export let name = ''
+    export let slug = ''
+    export let description = ''
+</script>
+  
+<div>
+    <img {src} alt={name} />
+    <a href={`/projects/${slug}`}>
+        <div>
+            <h2>{name}</h2>
+            <p>
+                {description.slice(0, 80)}...
+            </p>
+        </div>
+    </a>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,5 @@
 <script context="module">
+    import ProjectCard from '$lib/components/project-card.svelte';
     import { client } from '$lib/graphql-client';
     import { gql } from 'graphql-request'
   
@@ -32,4 +33,8 @@
     export let projects
 </script>
   
-<pre>{JSON.stringify(projects, null, 2)}</pre>
+<h1>Recent Projects by Me</h1>
+
+{#each projects as { name, slug, description, image }}
+    <ProjectCard {name} {description} src={image[0].url} {slug} />
+{/each}
