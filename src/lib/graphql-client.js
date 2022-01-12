20af7988035edc3b2212e6c1f0ae1987b19ac2a2
Gentil Pinto <gentil.junior.gp@gmail.com>
2022-01-12 11:12:50 +0100

    Show GraphQL data on index page

---
+++ src/lib/graphql-client.js
@@ -0,0 +1,6 @@
+import { gql, GraphQLClient } from 'graphql-request'
+
+const GRAPHQL_ENDPOINT = import.meta.env.VITE_GRAPHQL_API
+
+export const client = new GraphQLClient(GRAPHQL_ENDPOINT)
+
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+    import { client } from '$lib/graphql-client';
+    import { gql } from 'graphql-request'
+  
+    export const load = async () => {
+        const query = gql`
+            query GetProjects {
+                projects {
+                name
+                slug
+                description
+                demo
+                sourceCode
+                image {
+                    url
+                }
+                }
+            }
+        `
+
+        const { projects } = await client.request(query)
+
+        return {
+            props: {
+                projects,
+            },
+        }
+    }
+</script>
+
+<script>
+    export let projects
+</script>
+  
+<pre>{JSON.stringify(projects, null, 2)}</pre>
