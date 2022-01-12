530678f96684b4301f267347cba5b6f96589adc0
Gentil Pinto <gentil.junior.gp@gmail.com>
2022-01-12 14:55:00 +0100

    done!

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,13 @@
 <script>
     import Footer from '$lib/components/footer.svelte'
     import Navbar from '$lib/components/navbar.svelte'
+    import { onMount } from 'svelte'
+    import { themeChange } from 'theme-change'
     import '../app.css'
+  
+    onMount(async () => {
+      themeChange(false)
+    })
 </script>
   
 <Navbar />
